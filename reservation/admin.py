d5a6757fd3549e7ffddb1e4d8c6ec3112c926e75
ShutenkoStas <shotenko494@gmail.com>
2025-11-23 20:23:30 +0200

    Add models User and connect to admin.py

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from reservation.models import Booking, Room
+from reservation.models import Booking, Room, User
 
 
 admin.site.register(Booking)
 admin.site.register(Room)
+admin.site.register(User)
 
 # Register your models here.
--- reservation/models.py
+++ reservation/models.py
@@ -1,19 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 from django.utils import timezone
 
 class Room(models.Model):
     numder = models.IntegerField()
     capacity = models.IntegerField()
     locations = models.TextField()
+    type = models.CharField(max_length=222, null=True, blank=True)
 
     def __str__(self):
-        return f"Room #{self.numder} - {self.capacity}"
+        return f"Номер приміщення #{self.numder} - {self.capacity}, тип приміщення#{self.type}"
     
     class Meta:
-        verbose_name = "Room"
-        verbose_name_plural = "Rooms"
+        verbose_name = "Приміщення"
         ordering = ["numder"]
+
+class User(models.Model):
+    name = models.CharField(max_length=256)
+    sur_name = models.CharField(max_length=256)
+    all_rooms = models.ForeignKey(Room, on_delete=models.CASCADE)
+    email = models.EmailField(null=True, blank=True)
+    phone_number = models.CharField(max_length=30, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}{self.sur_name}"
+    
+    class Meta:
+        verbose_name = "Користувач/ка"
+        verbose_name_plural = "Користувачі"
+        ordering = ["name", "sur_name"]
 
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookings")
@@ -26,9 +41,12 @@ class Booking(models.Model):
         return f"{self.user}# Booking, {self.room}"
 
     class Meta:
-        verbose_name = "Booking"
-        verbose_name_plural = "Bookings"
+        verbose_name = "Резервування"
         ordering = ["start_time"]
+
+
+    
+
 
 
 
